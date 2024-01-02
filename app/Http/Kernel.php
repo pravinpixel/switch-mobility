9586d0b2c8b808ba697da9cd5cde99679b5726f7
pravin_pixel <pravin@pixel-studios.com>
2024-01-02 16:23:29 +0530

    02012024

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,7 +46,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             //new
             \App\Http\Middleware\AddSecurityHeaders::class,
-            \App\Http\Middleware\ValidateRedirects::class,
+           // \App\Http\Middleware\ValidateRedirects::class,
         ],
 
         'api' => [
