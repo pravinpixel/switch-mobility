e725debae5795b1b07e52a9d3f2df230eac6db05
pravin <pravin@pixel-studios.com>
2024-05-16 11:43:50 +0530

    16042024

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -44,11 +44,11 @@
     </style>
 </head>
 <body>
-    <section style="display:none;" class="page_404">
+    <section class="page_404">
         <div class="container">
             <div class="row">	
             <div class="col-sm-12 ">
-            <div class="col-sm-10 col-sm-offset-1  text-center">
+            <div style="display:none;"  class="col-sm-10 col-sm-offset-1  text-center">
             <div class="four_zero_four_bg">
                 <h1 class="text-center ">404</h1>
             
